9dbf3f0b2f564580ff63b071da1ae4b390b0d1af
Michael Waldo <mbwcogic@yahoo.com>
2020-01-26 01:12:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,9 @@
 class Backer 
   
-  attr_reader :name, :back_projects 
+  attr_reader :name, :backed_projects 
   
   def initialize(name)
     @name = name 
-    @back_projects = []
+    @backed_projects = []
   end 
 end
